5b9795392ffca9e38aae54d490669492698a251a
albanovs <albvnovs@gmail.com>
2023-09-25 12:38:08 +0600

    port is 5000

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json())
 
-const PORT = 4000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
